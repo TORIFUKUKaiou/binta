033a2609e811ef90e4caacba5674f6fe4feb0fa7
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2023-01-02 13:30:50 +0900

    bug fix

--- lib/binta/connpass.ex
+++ lib/binta/connpass.ex
@@ -4,12 +4,26 @@ defmodule Binta.Connpass do
   def event_now do
     utc_today = Date.utc_today()
 
-    ym =
-      "#{utc_today.year}#{utc_today.month |> Integer.to_string() |> String.pad_leading(2, "0")}"
+    ym = ym(utc_today.year, utc_today.month)
 
-    %{ym: ym, series_id: 11144}
+    events =
+      %{ym: ym, series_id: 11144}
+      |> run()
+      |> handle_response()
+
+    before_year = if utc_today.month == 1, do: utc_today.year - 1, else: utc_today.year
+
+    before_month =
+      Enum.zip(1..12, [12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+      |> Map.new()
+      |> Map.get(utc_today.month)
+
+    before_ym = ym(before_year, before_month)
+
+    %{ym: before_ym, series_id: 11144}
     |> run()
     |> handle_response()
+    |> Kernel.++(events)
     |> Enum.filter(fn %{"started_at" => started_at, "ended_at" => ended_at} ->
       {:ok, start, _} = DateTime.from_iso8601(started_at)
       {:ok, ending, _} = DateTime.from_iso8601(ended_at)
@@ -23,6 +37,10 @@ defmodule Binta.Connpass do
 
   def run(query) do
     Req.get("https://connpass.com/api/v1/event/?#{URI.encode_query(query)}")
+  end
+
+  defp ym(year, month) do
+    "#{year}#{month |> Integer.to_string() |> String.pad_leading(2, "0")}"
   end
 
   defp handle_response({:error, _}), do: []
--- lib/binta/slack.ex
+++ lib/binta/slack.ex
@@ -1,8 +1,10 @@
 defmodule Binta.Slack do
   @moduledoc false
+  require Logger
 
   def post(message) do
-    Req.post(url(), json: %{text: message}) |> IO.inspect()
+    post_result = Req.post(url(), json: %{text: message}) |> IO.inspect()
+    Logger.info(inspect(post_result))
   end
 
   defp url, do: Application.get_env(:binta, :slack_incoming_webhook_url)
