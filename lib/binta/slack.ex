efdbfc9e1a21dd4877cba0d6a3c42496c1015b65
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2023-01-02 13:04:27 +0900

    add IO.inspect

--- lib/binta/slack.ex
+++ lib/binta/slack.ex
@@ -2,7 +2,7 @@ defmodule Binta.Slack do
   @moduledoc false
 
   def post(message) do
-    Req.post(url(), json: %{text: message})
+    Req.post(url(), json: %{text: message}) |> IO.inspect()
   end
 
   defp url, do: Application.get_env(:binta, :slack_incoming_webhook_url)
